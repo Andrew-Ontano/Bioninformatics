ca4b21152439f7ad808b0a829b729f6199a1867c
andrew-ontano <andrew.ontano@gmail.com>
2022-03-10 13:20:34 -0600

    added xmfa2fa and triplet counter scripts

---
+++ scripts/alignment-triplet-counter.py
@@ -0,0 +1,63 @@
+#!/bin/python
+import sys
+import os
+from random import shuffle
+import argparse
+from Bio import SeqIO
+import csv
+from math import ceil, floor
+
+parser = argparse.ArgumentParser(description='Count the prevalence of 3-mers from an alignment')
+parser.add_argument('-i', dest='genome_fasta', type=str, help='Input genome fasta')
+parser.add_argument('-w', dest='window_width', type=int, help='Window width', default='1000')
+parser.add_argument('-s', dest='window_spacing', type=int, help='Window spacing', default='1000')
+parser.add_argument('-o', dest='report_output', type=str, help='Output file name', default='repeat_report.tsv')
+args = parser.parse_args()
+if not args.genome_fasta:
+    print("Please specify the proper input file")
+    exit()
+
+# make sure both file paths exist
+if not os.path.exists(args.genome_fasta):
+    print("File parameter is incorrect. Usage: 'python repeat-windows.py -i $genome_fasta -q '$query_sequence'")
+    exit()
+
+# load genome file
+genome = SeqIO.to_dict(SeqIO.parse(args.genome_fasta,'fasta'))
+# loop through remaining entries in order to find
+output = open(args.report_output, 'w',newline='')
+tsvWriter = csv.writer(output, delimiter='\t')
+
+window_lead = floor(args.window_width/2)
+window_tail = ceil(args.window_width/2)
+
+
+#bases = ["A","C","G","T","N","-"]
+bases = ["A","C","G","T"]
+queries = [bases[a]+bases[b]+bases[c] for c in range(0,len(bases)) for b in range(0,len(bases)) for a in range(0,len(bases))]
+
+tsvWriter.writerow(["Scaffold", "Start", "End"]+queries)
+
+for entry in genome:
+    i = window_lead
+    while i+window_tail < len(genome[entry]) :
+        rowSeq = str(genome[entry][i-window_lead:i+window_tail].seq)
+        rowCounts = [str(rowSeq.upper().count(query)) for query in queries]
+        rowText = [
+            entry,
+            i-window_lead+1,
+            i+window_tail
+        ]
+        rowText += rowCounts
+        tsvWriter.writerow(rowText)
+        i += args.window_spacing
+    rowSeq = str(genome[entry][i-window_lead:i+window_tail].seq)
+    rowCounts = [str(rowSeq.upper().count(query)) for query in queries]
+    rowText = [
+            entry,
+            i-window_lead+1,
+            len(genome[entry])
+    ]
+    rowText += rowCounts
+    tsvWriter.writerow(rowText)
+output.close()
---
+++ scripts/xmfa2fa.py
@@ -0,0 +1,20 @@
+#!/bin/python
+from Bio import AlignIO
+import gfapy
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Import mauve xmfa and output each alignment to separate file')
+parser.add_argument('-i', dest='input_xmfa', type=str, required=True, help='Input xmfa file')
+parser.add_argument('-o', dest='output_fasta', type=str, help='Output file prefix', default='mauve_alignments.fa')
+args = parser.parse_args()
+
+#Parse inputs as list of MSAs
+inputAlign = AlignIO.parse(args.input_xmfa,"mauve")
+alignments = list(inputAlign)
+
+with open(args.output_fasta, "w") as f:
+    for aln in alignments:
+        for record in aln:
+            f.write(">" + record.id.split('\\')[-1].replace(" ","_") + "\n")
+            f.write(str(record.seq) + "\n")
