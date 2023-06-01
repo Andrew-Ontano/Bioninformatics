915c8152a0a1e9bb11f11f8337053df345b920a3
Andrew-Ontano <andrew.ontano@gmail.com>
2023-06-01 10:20:53 -0500

    Fixed logging vcfFilter

--- scripts/vcfFilter.py
+++ scripts/vcfFilter.py
@@ -14,10 +14,9 @@ logger = logging.getLogger()
 parser = argparse.ArgumentParser(description='vcfFilter: a tool for processing vcf results by genotype information')
 parser.add_argument('-i', '--input', dest='input_vcf', type=str, help='Input VCF file', required=True)
 parser.add_argument('-o', '--output', dest='output_vcf', type=str, help='Output VCF file', required=True)
-#parser.add_argument('-g', '--genotypes', dest='genotypes_count', type=int, help='Number of genotypes', default=1)
-parser.add_argument('-t', '--targets', dest='target_genotypes', type=str, help='Genotype header names, separated by commas', required=True)
+parser.add_argument('-t', '--targets', dest='target_genotypes', type=str, help='Genotype names, separated by commas', required=True)
 parser.add_argument('-m', '--minimum-variant-size', dest='minimum_size', type=int, help='Minimum size of variants to include', default=1)
-parser.add_argument('-x', '--outgroup', dest='outgroup', type=str, help='Numeric values of outgroup. 0=reference', required=True)
+parser.add_argument('-x', '--outgroup', dest='outgroup', type=str, help='Outgroup name', required=True)
 parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Verbose mode', default=False)
 args = parser.parse_args()
 
@@ -28,12 +27,12 @@ if not os.path.exists(args.input_vcf):
     logging.error(f"Couldn't find input VCF '{args.input_vcf}'")
     exit()
 
-# Process the desire genotypes
-
+# Process the desired genotypes
 genotypeNames = {}
 outgroupNames = (None, None)
 
 with open(args.input_vcf, 'r') as vcfInput, open(args.output_vcf, 'w') as vcfOutput:
+    logger.info(f"VCF '{args.input_vcf} opened. Working through file.'")
     for line in vcfInput:
         if genotypeNames:
             lineSplit = line.strip().split('\t')
@@ -67,57 +66,5 @@ with open(args.input_vcf, 'r') as vcfInput, open(args.output_vcf, 'w') as vcfOut
                     outgroupNames = (0, args.outgroup)
                 if args.outgroup in genotypeNames:
                     genotypeNames.pop(args.outgroup)
-
-
-
-# # set a tuple with the boundaries for genotypes
-# genotypeColumns = [a+9 for a in range(0, args.genotypes_count)]
-# targetColumns = args.target_columns.split(',')
-# # switch to reference mode if reference included, or outgroup mode if reference is the outgroup
-# if '0' in targetColumns:
-#     genotypeTargetColumns = [genotypeColumns[int(a)-1] for a in targetColumns if a != '0']
-#     outgroupColumn = 9 + args.outgroup_column - 1
-#     with open(args.input_vcf, 'r') as vcfInput, open(args.output_vcf, 'w') as vcfOutput:
-#         for line in vcfInput:
-#             if line.startswith('#'):
-#                 vcfOutput.write(line)
-#             else:
-#                 lineSplit = line.strip().split('\t')
-#                 genotypeValues = [lineSplit[a] for a in genotypeTargetColumns]
-#                 outgroupValue = lineSplit[outgroupColumn]
-#                 # make sure variants are called
-#                 if '.' not in genotypeValues and outgroupValue != '.':
-#                     # add the reference ('0') to the list before making the collapsible list
-#
-#                     if (outgroupValue in genotypeValues or outgroupValue == '0') and len(set(['0'] + genotypeValues)) > 1:
-#                         variants = [lineSplit[3]] + lineSplit[4].split(',')
-#                         if len([a for a in list(set(['0'] + genotypeValues)) if len(variants[int(a)]) >= args.minimum_size]) > 0:
-#                             #print(line)
-#                             vcfOutput.write(line)
-# elif args.outgroup_column == 0:
-#     logging.error("This is not supported yet, sorry!")
-#
-#     genotypeTargetColumns = [genotypeColumns[int(a)-1] for a in targetColumns if a != '0']
-#     with open(args.input_vcf, 'r') as vcfInput, open(args.output_vcf, 'w') as vcfOutput:
-#         for line in vcfInput:
-#             if line.startswith('#'):
-#                 vcfOutput.write(line)
-#     pass
-# else:
-#     genotypeTargetColumns = [genotypeColumns[int(a)-1] for a in args.target_columns.split(',')]
-#     outgroupColumn = 9 + args.outgroup_column - 1
-#     with open(args.input_vcf, 'r') as vcfInput, open(args.output_vcf, 'w') as vcfOutput:
-#         for line in vcfInput:
-#             if line.startswith('#'):
-#                 vcfOutput.write(line)
-#             else:
-#                 lineSplit = line.strip().split('\t')
-#                 genotypeValues = [lineSplit[a] for a in genotypeTargetColumns]
-#                 outgroupValue = lineSplit[outgroupColumn]
-#
-#                 # make sure variants are called
-#                 if '.' not in genotypeValues and outgroupValue != '.':
-#                     if outgroupValue in genotypeValues and len(set(genotypeValues)) > 1:
-#                         variants = [lineSplit[3]] + lineSplit[4].split(',')
-#                         if len([a for a in list(set(genotypeValues)) if len(variants[int(a)]) >= args.minimum_size]) > 0:
-#                             vcfOutput.write(line)
+                logger.info(f"Genotype names and info processed.\nGenotypes (Dictionary of name / column indices):{genotypeNames}; Outgroup (tuple of name / column index): {outgroupNames}.\nBeginning variant processing...")
+    logger.info(f"VCF '{args.input_vcf}' has been fully processed. Filtered VCF written to '{args.output_vcf}'")
